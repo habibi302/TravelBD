8df9ecab8f779e44eea621252b1de0a4f7d49316
TECHSTODGY\Habib <techstodgy@gmail.com>
2022-10-07 23:29:34 +0600

    add constent

---
+++ client/src/constants/carousels_photos.js
@@ -0,0 +1,6 @@
+const carousels_photos = [
+{
+    src:"https://wallpaperaccess.com/full/2630258.jpg"
+}];
+
+export default carousels_photos;
---
+++ client/src/constants/posts.js
@@ -0,0 +1,196 @@
+const data = [
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    },
+    {
+        title:"Title",
+        content:"Hey My name is Name, and I am from Bangladesh",
+        url:"https://healinghutclinic.com/wp-content/uploads/2021/11/320.png"
+    }
+    
+]
+
+
+export default data;
